d7bd36e882ecbabbd779c558c8131e4aff426c05
jyang126 <uuuu99199@gmail.com>
2015-08-26 18:45:34 -0400

    Create Roman to Integer.java

---
+++ Roman to Integer.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int map(char c){
+		switch (c){
+		case 'I': return 1;
+		case 'V': return 5;
+		case 'X': return 10;
+		case 'L': return 50;
+		case 'C': return 100;
+		case 'D': return 500;
+		case 'M': return 1000;
+		default: return 0;
+		}
+	}
+    public int romanToInt(String s) {
+        if (s==null||s.length()==0)
+        return 0;
+        int pre=0;
+        int result=0;
+        for (int i=0;i<s.length();i++){
+            int curr=map(s.charAt(i));
+            result+=curr;
+            if (curr>pre) {
+                result-=2*pre;
+            }
+            pre=curr;
+        }
+        return result;
+    }
+}
